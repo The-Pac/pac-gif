822f031a19c4eaa2c406d86292379044896abab4
baptiste arsac <baptiste.arsac@astree-software.fr>
2023-04-18 15:58:29 +0200

    test de capture

--- src-tauri/src/commands/app_commands.rs
+++ src-tauri/src/commands/app_commands.rs
@@ -1,7 +1,39 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::BufWriter;
+use image::ColorType::Rgb8;
+use image::{ImageBuffer, ImageFormat};
+use log::info;
 use tauri::command;
-use crate::libs::gif_creator::capture_area;
+use crate::libs::gif_creator::capture_screen;
+
+fn bytes_to_png(bytes: &[u8], width: u32, height: u32) -> Result<(), Box<dyn Error>> {
+    let img_buf = ImageBuffer::<image::Rgba<u8>, _>::from_vec(width, height, bytes.to_vec())
+        .ok_or("Failed to create ImageBuffer")?;
+
+    let file = File::create("image.png")?;
+    let ref mut buffer_writer = BufWriter::new(file);
+
+    img_buf.write_to(buffer_writer, ImageFormat::Png)?;
+
+    Ok(())
+}
 
 #[command]
 pub fn take_gif(x: i32, y: i32, width: i32, height: i32) {
-    capture_area(x, y, width, height)
+    match capture_screen(x, y, width, height) {
+        Ok(buffer_image) => {
+            info!("{:?}",buffer_image);
+            match bytes_to_png(&buffer_image, width as u32, height as u32) {
+                Ok(()) => {
+                    println!("Image saved as PNG");
+                }
+                Err(e) => {
+                    eprintln!("Error: {}", e);
+                }
+            }
+            info!("ok")
+        }
+        Err(_) => {}
+    };
 }
--- src-tauri/src/libs/gif_creator.rs
+++ src-tauri/src/libs/gif_creator.rs
@@ -1,95 +1,55 @@
-use std::{mem, ptr};
-use std::error::Error;
-use std::io::Write;
-use winapi::um::winuser::{GetClientRect, GetDC, ReleaseDC, ShowWindow, SW_HIDE, SW_RESTORE};
-use winapi::shared::windef::{HDC, HWND, RECT};
-use winapi::um::wingdi::{SRCCOPY, StretchDIBits};
+use std::ffi::c_void;
+use std::io::Error;
+use std::mem;
+use std::mem::size_of;
+use std::ptr;
+use std::ptr::null_mut;
+use winapi::shared::windef::{HBITMAP, HGDIOBJ};
+use winapi::um::wingdi::{BI_RGB, BitBlt, BITMAPINFO, BITMAPINFOHEADER, CreateCompatibleBitmap, CreateCompatibleDC, DeleteDC, DeleteObject, DIB_RGB_COLORS, GetDeviceCaps, GetDIBits, SelectObject, SRCCOPY};
+use winapi::um::winuser::{GetDC, GetDesktopWindow, GetWindowDC, ReleaseDC};
 
+pub fn capture_screen(x: i32, y: i32, width: i32, height: i32) -> Result<Vec<u8>, Error> {
+    let mut bmp_info: BITMAPINFO = unsafe { mem::zeroed() };
+    bmp_info.bmiHeader.biSize = size_of::<BITMAPINFOHEADER>() as u32;
+    bmp_info.bmiHeader.biWidth = width;
+    bmp_info.bmiHeader.biHeight = height;
+    bmp_info.bmiHeader.biPlanes = 1;
+    bmp_info.bmiHeader.biBitCount = 32;
+    bmp_info.bmiHeader.biCompression = BI_RGB;
 
-pub fn capture_area(x: i32, y: i32, width: i32, height: i32) -> Result<Image, ()> {
-    // Obtenir le handle de la fenêtre active
-    let window = unsafe { winapi::um::winuser::GetForegroundWindow() };
+    let hdc_screen = unsafe { GetDC(null_mut()) };
+    let hdc = unsafe { CreateCompatibleDC(hdc_screen) };
+    let hbitmap = unsafe { CreateCompatibleBitmap(hdc_screen, width, height) };
+    let hbitmap_old = unsafe { SelectObject(hdc, hbitmap as *mut _) };
 
-    // Obtenir le contexte de périphérique de la fenêtre
-    let hdc: HDC = unsafe { GetDC(window) };
-
-    // Obtenir les dimensions de la fenêtre
-    let mut rect: RECT = unsafe { mem::zeroed() };
-    unsafe { GetClientRect(window, &mut rect) };
-
-    // Créer un tampon pour stocker les données de l'image
-    let size = (rect.right - rect.left) * (rect.bottom - rect.top) * 4;
-    let mut buffer: Vec<u8> = Vec::with_capacity(size as usize);
-
-    // Copier les données de l'image dans le tampon
-    let result = unsafe {
-        StretchDIBits(
-            hdc,
-            0,
-            0,
-            width,
-            height,
-            0,
-            0,
-            width,
-            height,
-            ptr::null_mut(),
-            &mut buffer,
-            0,
-            SRCCOPY,
-        )
-    };
-
-    // Libérer le contexte de périphérique de la fenêtre
-    unsafe { ReleaseDC(window, hdc) };
-
-    // Vérifier si la capture d'écran a réussi
-    if result == 0 {
-        return Err("Impossible de prendre la capture d'écran".into());
+    unsafe {
+        BitBlt(hdc, 0, 0, width, height, hdc_screen, x, y, SRCCOPY);
     }
 
-    // Écrire les données de l'image dans un fichier
-    let mut file = std::fs::File::create("screenshot.bmp")?;
-    let bmp_header = create_bmp_header(&rect)?;
-    file.write_all(&bmp_header)?;
-    file.write_all(&buffer)?;
+    let mut buffer: Vec<u8> = Vec::with_capacity((width * height * 4) as usize);
+    let result = unsafe {
+        let buffer_ptr = buffer.as_mut_ptr() as *mut c_void;
+        let hbitmap_obj = hbitmap as HBITMAP;
+        let ret = GetDIBits(hdc, hbitmap_obj, 0, height as u32, buffer_ptr, &mut bmp_info, DIB_RGB_COLORS);
+        if ret == 0 {
+            Err(Error::last_os_error())
+        } else {
+            Ok(())
+        }
+    };
+    if let Err(e) = result {
+        return Err(e);
+    }
 
+    let stride = (width * 4) as usize;
+    buffer.resize(stride * height as usize, 0);
 
-    Ok(())
+    unsafe {
+        SelectObject(hdc, hbitmap_old as *mut _);
+        DeleteObject(hbitmap as HGDIOBJ);
+        DeleteDC(hdc);
+        ReleaseDC(null_mut(), hdc_screen);
+    }
+
+    Ok(buffer)
 }
-
-fn create_bmp_header(rect: &RECT) -> Result<Vec<u8>, Box<dyn Error>> {
-    let file_size = (rect.right - rect.left) * (rect.bottom - rect.top) * 4 + 54;
-    let bmp_header = vec![
-        b'B', b'M', // Magic number
-        (file_size & 0xFF) as u8, // Taille du fichier en octets (1)
-        (file_size >> 8 & 0xFF) as u8, // Taille du fichier en octets (2)
-        (file_size >> 16 & 0xFF) as u8, // Taille du fichier en octets (3)
-        (file_size >> 24 & 0xFF) as u8, // Taille du fichier en octets (4)
-        0, 0, 0, 0, // Réservé
-        54, 0, 0, 0, // Offset des données de l'image
-        40, 0, 0,
-        (rect.right - rect.left) as u8, // Taille de l'en-tête BITMAPINFOHEADER (1)
-        (rect.right - rect.left >> 8) as u8, // Taille de l'en-tête BITMAPINFOHEADER (2)
-        (rect.right - rect.left >> 16) as u8, // Taille de l'en-tête BITMAPINFOHEADER (3)
-        (rect.right - rect.left >> 24) as u8, // Taille de l'en-tête BITMAPINFOHEADER (4)
-        (rect.bottom - rect.top) as u8, // Largeur de l'image (1)
-        (rect.bottom - rect.top >> 8) as u8, // Largeur de l'image (2)
-        (rect.bottom - rect.top >> 16) as u8, // Largeur de l'image (3)
-        (rect.bottom - rect.top >> 24) as u8, // Largeur de l'image (4)
-        1, 0, // Nombre de plans (1)
-        0, 0, // Nombre de plans (2)
-        24, 0, // Nombre de bits par pixel
-        0, 0, 0, 0, // Compression
-        0, 0, 0, 0, // Taille des données de l'image
-        0, 0, 0, 0, // Résolution horizontale
-        0, 0, 0, 0, // Résolution verticale
-        0, 0, 0, 0, // Nombre de couleurs dans la palette
-        0, 0, 0, 0, // Nombre de couleurs importantes
-    ];
-    Ok(bmp_header)
-}
-
-
-
-
